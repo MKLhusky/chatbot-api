c5af274f50a6068844c479bffa5170f41b9e5298
范文鹏 <fanwenpeng@51rcar.com>
2023-10-13 16:15:04 +0800

    初始化仓库

---
+++ chatbot-api-interfaces/src/main/java/cn/best/fwp/ApiApplication.java
@@ -0,0 +1,13 @@
+package cn.best.fwp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
